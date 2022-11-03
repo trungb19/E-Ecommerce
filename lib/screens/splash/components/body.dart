165ea9f889dc1dca9ef16522faecf4cd59872aeb
trungb19 <trungb1910322@gmail.com>
2022-11-03 14:38:49 +0700

    Xay dung trang giao dien dau tien cua ung dung thuong mai dien tu

--- lib/screens/splash/components/body.dart
+++ lib/screens/splash/components/body.dart
@@ -3,6 +3,8 @@ import 'package:shop_app/constants.dart';
 import 'package:shop_app/screens/splash/components/splash_content.dart';
 import 'package:shop_app/size_config.dart';
 
+import '../../../components/default_button.dart';
+
 class Body extends StatefulWidget {
   @override
   _BodyState createState() => _BodyState();
@@ -35,6 +37,9 @@ class _BodyState extends State<Body> {
             Expanded(
               flex: 3,
               child: PageView.builder(
+                onPageChanged: (value) => setState(() {
+                  currentPage = value;
+                }),
                 itemCount: splashData.length,
                 itemBuilder: (context, index) => SplashContent(
                   image: splashData[index]["image"].toString(),
@@ -44,13 +49,23 @@ class _BodyState extends State<Body> {
             ),
             Expanded(
               flex: 2,
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    children: List.generate(
-                        splashData.length, (index) => builtDot(index)),
-                  )
-                ],
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate(
+                          splashData.length, (index) => builtDot(index)),
+                    ),
+                    Spacer(flex: 2),
+                    DefaultButton(
+                      text: "Continue",
+                      press: () {},
+                    ),
+                    Spacer(),
+                  ],
+                ),
               ),
             ),
           ],
@@ -59,8 +74,9 @@ class _BodyState extends State<Body> {
     );
   }
 
-  Container builtDot(int index) {
-    return Container(
+  AnimatedContainer builtDot(int index) {
+    return AnimatedContainer(
+      duration: kAnimationDuration,
       margin: EdgeInsets.only(right: 5),
       height: 6,
       width: currentPage == index ? 20 : 6,
--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -8,6 +8,7 @@ import 'package:shop_app/screens/splash/components/body.dart';
 import 'package:shop_app/size_config.dart';
 
 class SplashScreen extends StatelessWidget {
+  static String routeName = "/splash";
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
